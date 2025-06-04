7d463f050b9fc6d7216de7d0a2939a56a3b5519a
weizhi33 <a37705040@gmail.com>
2025-06-04 15:09:44 +0800

    Update HOTIsland.py

--- pages/HOTIsland.py
+++ pages/HOTIsland.py
@@ -1,3 +1,25 @@
+import streamlit as st
+import ee
+from google.oauth2 import service_account
+import geemap.foliumap as geemap
+
+# 從 Streamlit Secrets 讀取 GEE 服務帳戶金鑰 JSON
+service_account_info = st.secrets["GEE_SERVICE_ACCOUNT"]
+
+# 使用 google-auth 進行 GEE 授權
+credentials = service_account.Credentials.from_service_account_info(
+    service_account_info,
+    scopes=["https://www.googleapis.com/auth/earthengine"]
+)
+
+# 初始化 GEE
+ee.Initialize(credentials)
+
+###############################################
+
+st.set_page_config(layout="wide")
+st.title("衛星與Sentinel-2土地覆蓋之K-means分群🌍")
+
 import ee
 ee.Authenticate()
 ee.Initialize(project="ee-s1143056")
