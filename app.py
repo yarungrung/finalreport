195b12fe19a9976ef6e7f3ee7547022703ad7492
yarungrung <s1243032@gm.ncue.edu.tw>
2025-06-05 00:00:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,36 +12,12 @@ st.markdown(
     """
 )
 
-st.header("Instructions")
+st.title("南科發展史")
 
 
-st.title("選擇日期區間")
-
-
-# 初始化 session_state
-#if 'start_date' not in st.session_state:
-#    st.session_state['start_date'] = date(2024, 1, 1)
-#if 'end_date' not in st.session_state:
-#    st.session_state['end_date'] = date.today()
-
-st.session_state['start_date'] = date(2024, 1, 1)
-st.session_state['end_date'] = date.today()
-
-
-# 日期選擇器
-start_date = st.date_input(label = "選擇起始日期", value = st.session_state['start_date'], min_value = date(2018, 1, 1), max_value = date.today())
-end_date = st.date_input(label = "選擇結束日期", value = st.session_state['end_date'], min_value = start_date, max_value = date.today())
-
-# 儲存使用者選擇
-st.session_state['start_date'] = start_date
-st.session_state['end_date'] = end_date
-
-st.success(f"目前選擇的日期區間為：{start_date} 到 {end_date}")
 
 st.title("展示timelapse檔")
-
 st.markdown("### 南科 Timelapse")
-
 
 # 顯示三張並排的 timelapse
 col1, col2, col3 = st.columns(3)
