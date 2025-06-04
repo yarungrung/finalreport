e6e97899495cb4421d3572193cc421cac0685c10
yarungrung <s1243032@gm.ncue.edu.tw>
2025-06-04 17:08:17 +0800

    Create timelapse.py

---
+++ pages/timelapse.py
@@ -0,0 +1,15 @@
+st.markdown("### 南科 Timelapse")
+
+st.image("Tainansmall1984~2024.png")
+st.image("Tainannewroad.png")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.image("1894-2000 南科timelapse.png", caption="1894-2000")
+
+with col2:
+    st.image("2001-2019 南科timelapse.png", caption="2001-2019")
+
+with col3:
+    st.image("2020-2025 南科timelapse.png", caption="2020-2025")
