6528b95976af165907d6af6c3f1fbed9143b432a
yarungrung <s1243032@gm.ncue.edu.tw>
2025-06-04 15:23:16 +0800

    Update satellite_twcastlemap.py

--- pages/satellite_twcastlemap.py
+++ pages/satellite_twcastlemap.py
@@ -27,27 +27,39 @@ aoi = my_point.buffer(1000)  # 1000 公尺緩衝區作為感興趣區域
 # 建立地圖
 my_Map = geemap.Map()
 
-# 用landset5匯入1984的真色衛星影像
+# 1984 年：Landsat 5
 my_image1984 = ee.ImageCollection('LANDSAT/LT05/C01/T1_SR') \
     .filterDate('1984-01-01', '1984-12-31') \
     .filterBounds(aoi) \
     .sort('CLOUD_COVER') \
     .first()
 
-# 擷取 Harmonized Sentinel-2 MSI: MultiSpectral Instrument, Level-1C 2025衛星影像
-my_image1984 = ee.ImageCollection('LANDSAT/LT05/C01/T1_SR') \
+# 2024 年：Sentinel-2
+my_image2024 = ee.ImageCollection('COPERNICUS/S2_SR') \
     .filterDate('2024-01-01', '2024-12-31') \
     .filterBounds(aoi) \
-    .sort('CLOUD_COVER') \
+    .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20)) \
+    .sort('CLOUDY_PIXEL_PERCENTAGE') \
     .first()
 
-vis_params = {'min': 0, 'max': 3000, 'bands': ['B3', 'B2', 'B1']}
+# 檢查是否有成功取得影像
+if not my_image1984:
+    st.error("❌ 找不到符合條件的 1984 年影像。")
+    st.stop()
 
-left_layer = geemap.ee_tile_layer(my_image1984,vis_params, '1984真色')
-right_layer = geemap.ee_tile_layer(my_image2025,vis_params, '2025真色')
+if not my_image2024:
+    st.error("❌ 找不到符合條件的 2024 年影像。")
+    st.stop()
 
+# 視覺化參數
+vis_params_1984 = {'min': 0, 'max': 3000, 'bands': ['B3', 'B2', 'B1']}  # Landsat
+vis_params_2024 = {'min': 0, 'max': 3000, 'bands': ['B4', 'B3', 'B2']}  # Sentinel-2
+
+# 圖層
+left_layer = geemap.ee_tile_layer(my_image1984, vis_params_1984, '1984 真色')
+right_layer = geemap.ee_tile_layer(my_image2024, vis_params_2024, '2024 真色')
+
+# 地圖顯示
 my_Map.centerObject(aoi, 12)
 my_Map.split_map(left_layer, right_layer)
-
-# 顯示地圖
 my_Map.to_streamlit(height=600)
