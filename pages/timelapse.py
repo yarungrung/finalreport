8b0d376b6cc4a0871f40e685213b90e1eea324cc
yarungrung <s1243032@gm.ncue.edu.tw>
2025-06-04 19:36:12 +0800

    Update timelapse.py

--- pages/timelapse.py
+++ pages/timelapse.py
@@ -1,8 +1,13 @@
-st.markdown("### 南科 Timelapse")
+import streamlit as st
+st.set_page_config(layout="wide")  # 若你有用到寬版顯示
+st.title("南科 Timelapse 比較展示")
 
+st.markdown("### 南科 Timelapse")
+# 顯示前兩張全幅圖片
 st.image("Tainansmall1984~2024.png")
 st.image("Tainannewroad.png")
 
+# 顯示三張並排的 timelapse
 col1, col2, col3 = st.columns(3)
 
 with col1:
