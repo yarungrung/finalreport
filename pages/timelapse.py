7af2fe1e090089c02c1f674aa8457ebd2257b614
yarungrung <s1243032@gm.ncue.edu.tw>
2025-06-05 00:39:47 +0800

    Update timelapse.py

--- pages/timelapse.py
+++ pages/timelapse.py
@@ -2,9 +2,11 @@ import streamlit as st
 st.set_page_config(layout="wide")  
 st.title("南科 Timelapse 比較展示")
 
-st.markdown("### 南科 Timelapse")
+st.markdown("### 以Timelapse展現從普通農地逐步興建成「南科」過程")
 # 顯示前兩張全幅圖片
 st.image("Tainansmall1984~2024.png")
+
+st.markdown("### 以Timelapse「南科」從零到有的全過程及周遭尤其路網的同步發展")
 st.image("Tianannewroad.png")
 
 # 顯示三張並排的 timelapse
