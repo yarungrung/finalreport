ca25323c814b13dc5085a4479a8d11d3bf48e91f
yarungrung <s1243032@gm.ncue.edu.tw>
2025-06-04 17:01:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,15 @@ st.success(f"目前選擇的日期區間為：{start_date} 到 {end_date}")
 
 st.title("展示timelapse檔")
 
-st.markdown("### 南科 Timelapse")  # 大標題
-st.image("1894-2000 南科timelapse.png")
-st.image("2001-2019 南科timelapse.png")
-st.image("2020-2025 南科timelapse.png")
+st.markdown("### 南科 Timelapse")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.image("1894-2000 南科timelapse.png", caption="1894-2000")
+
+with col2:
+    st.image("2001-2019 南科timelapse.png", caption="2001-2019")
+
+with col3:
+    st.image("2020-2025 南科timelapse.png", caption="2020-2025")
