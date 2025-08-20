ebb48e2fa7e8c423cb6106a90672bf75677b183a
yarungrung <s1243032@gm.ncue.edu.tw>
2025-08-20 08:05:35 +0800

    Update 南科 Timelapse 比較展示.py

--- pages/南科 Timelapse 比較展示.py
+++ pages/南科 Timelapse 比較展示.py
@@ -3,12 +3,12 @@ st.set_page_config(layout="wide")
 st.title("南科 Timelapse 比較展示")
 
 # 顯示前兩張全幅圖片
-st.markdown(" 以Timelapse展現從普通農地逐步興建成「南科」過程            以Timelapse南科從零到有的全過程及周遭尤其路網的同步發展")
+st.markdown("1984-2025之衛星圖像變化")
 col1, col2 = st.columns(2)
 with col1:
-    st.image("Tainansmall1984~2024.png")
+    st.image("Tainansmall1984~2024.png", caption="1984-2025南科發展過程")
 with col2:
-    st.image("Tianannewroad.png")
+    st.image("Tianannewroad.png", caption="1984-2025南科與周遭路網發展過程")
 
 # 顯示三張並排的 timelapse
 st.markdown(" 依據南科發展史分割三大時期，從左到右分別為：從無到南科第一期的擴建、南科從第一期至第二期的擴建、南科從第二期至第三期的擴建及至今")
